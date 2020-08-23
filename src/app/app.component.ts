4678c884b1cbed24ca35649b9b88a3a8a526a99f
Livsnekad42 <savelevartem5@gmail.com>
2020-08-23 14:37:20 +0700

    create base file structure form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular';
+  myPosts: Post[] = [
+    { title: 'First post', text: 'First text'},
+    { title: 'Second post', text: 'Second text'}
+  ];
 }
